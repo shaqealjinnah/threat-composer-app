3198dac0c69ca8f6eace8bb1f21c2cd94b86f4c4
shaqealjinnah <j.shaqeal@gmail.com>
2026-04-04 19:15:29 +1100

    feat(networking): add NAT gateways for private subnet outbound internet access

--- infra/modules/networking/main.tf
+++ infra/modules/networking/main.tf
@@ -43,12 +43,12 @@ resource "aws_internet_gateway" "igw" {
     }
 }
 
-# Create Route Table and Configure Routes
+# Create Public Route Table and Configure Routes
 resource "aws_route_table" "public" {
     vpc_id = aws_vpc.main.id
 
     tags = {
-        Name = "${var.project_name}--route_table"
+        Name = "${var.project_name}--public-route_table"
     }
 }
 
@@ -63,4 +63,52 @@ resource "aws_route_table_association" "public" {
 
     subnet_id = aws_subnet.public[count.index].id
     route_table_id = aws_route_table.public.id
+}
+
+resource "aws_eip" "nat" {
+    count = 2
+    domain = "vpc"
+
+    tags = {
+        Name = "${var.project_name}--eip-${count.index + 1}"
+    }
+
+}
+
+resource "aws_nat_gateway" "public" {
+    count = length(aws_subnet.public)
+
+    subnet_id = aws_subnet.public[count.index].id
+    allocation_id = aws_eip.nat[count.index].id
+
+    depends_on = [aws_internet_gateway.igw]
+
+    tags = {
+        Name = "${var.project_name}--nat-gateway-${count.index + 1}"
+    }
+}
+
+# Create Private Route Table and Configure Routes
+resource "aws_route_table" "private" {
+    count = length(aws_subnet.private)
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+        Name = "${var.project_name}--private-route_table-${count.index + 1}"
+    }
+}
+
+resource "aws_route" "private_nat" {
+    count = length(aws_nat_gateway.public)
+
+    route_table_id = aws_route_table.private[count.index].id
+    destination_cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.public[count.index].id
+}
+
+resource "aws_route_table_association" "private" {
+    count = length(aws_subnet.private)
+
+    subnet_id = aws_subnet.private[count.index].id
+    route_table_id = aws_route_table.private[count.index].id
 }
