7c201428b84a821a73353e91fb77a1b4960ba79c
shaqealjinnah <j.shaqeal@gmail.com>
2026-04-16 18:59:46 +1000

    feat(ecs): add Fargate task definition for application container

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -12,3 +12,32 @@ resource "aws_ecs_cluster" "main" {
     }
 }
 
+# Define Task Definition Blueprint
+resource "aws_ecs_task_definition" "main" {
+    family = "main"
+    requires_compatibilities = ["FARGATE"]
+    network_mode = "awsvpc"
+
+    cpu = "256"
+    memory = "512"
+
+    execution_role_arn = var.ecs_task_execution_arn
+    task_role_arn = var.ecs_task_role_arn
+
+    container_definitions = jsonencode([
+        {
+            name = "app"
+            image = "${var.image_url}:latest"
+            essential = true
+            portMappings = [
+                {
+                    containerPort = var.container_port
+                    hostPort = var.container_port
+                }
+            ]
+        }])
+
+    tags = {
+        Name =  "${var.project_name}--task-definition"
+    }
+}
